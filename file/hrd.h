7556cb2fcde8826360c4d903bbed648643716d3f
Nuwan Rajika Kumarasiri <nuwan.kumarasiri@wisc.edu>
2019-10-17 13:46:09 -0500

    add common header

---
+++ file/hrd.h
@@ -0,0 +1,17 @@
+#ifndef HRD_H
+#define HRD_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+typedef enum {FALSE, TRUE} bool;
+
+#define min(m,n) ((m) < (n)? (m):(n))
+#define max(m,n) ((m) > (n)? (m):(n))
+
+#endif
