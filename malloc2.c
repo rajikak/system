fb3e882a2c22905106e714bc70df272c553a0930
Nuwan Rajika Kumarasiri <nuwan.kumarasiri@wisc.edu>
2020-05-25 17:37:35 -0500

    malloc/free impl.

--- malloc2.c
+++ malloc2.c
@@ -1,80 +1,77 @@
-/* basic malloc() and free() for `char` type */
+/* malloc() and free() for `char` type */
 #include "hdr.h"
 
 typedef struct node {
 	size_t len;
 	struct node *prev;
 	struct node *next;
-	char *buf;
+	char   *buf;
 } node_t;
 
-/* head of free list(list of memory free by free) */
+/* free list */
 static node_t *head; 
 
 char *malloc2(size_t size){
 	char *p;
-	/* build free list, a more efficient allocator can move the 
-	 * break point in much bigger chunks than requested.*/
-	printf("program break: %10p\n", sbrk(0));
+	int counter = 0;
+	printf("program break(counter=%d): %10p\n", counter++, sbrk(0));
 	if (head == NULL) {
-		node_t e;
-		e.len  = size;
-		e.prev = NULL;
-		e.next = NULL;
-
-		p = (char *)sbrk(size + 1); /* extra byte for '\0' */
+		p = (char *)sbrk(size + 2); /* extra two bytes for '\0' and size */
 		if (p == (char *) -1) {
 			fprintf(stderr, "sbrk failed, errno=%d\n", errno);
 			return NULL;
 		}
-		e.buf = p;
-		head = &e;
-
-		printf("break now    : %10p\n", sbrk(0));
-		return e.buf;
+		*p = (char) size;
+		printf("program break(counter=%d): %10p\n", counter++, sbrk(0));
+		return p + 1;
 		
 	} else {
-		/* best fit allocator */
+		/* first fit allocator */
 		node_t *tmp = head;
 		while(tmp != NULL) {
 
 			if(tmp->len > size) {
 				tmp->prev->next = tmp->next;
 				tmp->next->prev = tmp->prev;
-				printf("program break(after allocation-best fit): %10p\n", sbrk(0));
-				return tmp;
+				printf("program break(counter=%d): %10p\n", counter++, sbrk(0));
+				return tmp->buf;
 			}	
 
 			tmp = tmp->next;		
 		}
 
-		node_t e;
-		e.len = size;
-		e.prev = head->next;
-		e.next = NULL; /* tail */
-		p = (char *)sbrk(size + 1);
-		if ( p == (char *) -1) {
+		p = (char *)sbrk(size + 2);
+		if (p == (char *) -1) {
 			fprintf(stderr, "sbrk failed, errono=%d\n", errno);
 			return NULL;
 		}
 
-		e.buf = p;
-		tmp->next = &e;
-
-		printf("program break(after allocation): %10p\n", sbrk(0));
-		return e.buf;
+		*p = (char) size;
+		printf("program break(counter=%d): %10p\n", counter++, sbrk(0));
+		return p + 1;
 	}
 }
 
 void free2(char *ptr){
-	node_t *tmp = head;
 
-	/* double free is seg fault */
-	while(tmp != NULL) {
-		tmp->next = 		
+	node_t e, *tmp;
+	e.len = *(ptr - 1);
+	e.next = NULL; /* tail */
+	e.buf = NULL; /* derefrence */
 
-		tmp = tmp->next;
+	if (head == NULL){
+		e.prev = NULL;
+		head = &e;
+	} else {
+		/* append to free list */
+		tmp = head;
+		while(tmp->next != NULL) {
+			tmp = tmp->next;
+		}
+		tmp->next = &e;
+		e.prev = tmp;
 	}
+	ptr = NULL;
 }
 
 
@@ -85,17 +82,17 @@ int main() {
 	buf1 = malloc2(5); /* "hello" */
 	buf2 = malloc2(5); /* "world" */
 
-	printf("buf1: %10p\n", buf1);
-	printf("buf2: %10p\n", buf2);
-
 	memcpy(buf1, "hello", 5);
 	memcpy(buf2, "world", 5);
+
+	printf("buf1: %s\n", buf1);
+	printf("buf2: %s\n", buf2);
 
 	free2(buf1);
 	free2(buf2);
 
 	/* seg fault, double free */
-	printf("%s\n", buf1);
+	printf("buf1: %s\n", buf1);
 
 	return 0;
 }
